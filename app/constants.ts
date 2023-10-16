83e77e8c79e7093b4e193ae677a7bb971de71449
moraesc <cmoraes1955@gmail.com>
2023-10-16 15:26:34 -0400

    First commit

---
+++ app/constants.ts
@@ -0,0 +1,50 @@
+export const CATEGORIES = [
+  {
+    name: "Age",
+    key: "age",
+  },
+  {
+    name: "Alone",
+    key: "alone",
+  },
+  {
+    name: "Amazing",
+    key: "amazing",
+  },
+  {
+    name: "Anger",
+    key: "anger",
+  },
+  {
+    name: "Art",
+    key: "art",
+  },
+  {
+    name: "Attitude",
+    key: "attitude",
+  },
+  {
+    name: "Beauty",
+    key: "beauty",
+  },
+  {
+    name: "Best",
+    key: "best",
+  },
+  {
+    name: "Birthday",
+    key: "birthday",
+  },
+  {
+    name: "Business",
+    key: "business",
+  },
+  {
+    name: "Dreams",
+    key: "dreams",
+  },
+  {
+    name: "Education",
+    key: "education",
+  },
+];
--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,68 @@
-import Image from 'next/image'
+"use client";
+
+import React, { useState } from "react";
+import { CATEGORIES } from "./constants";
+
+const API_KEY = "iLfF0bPXjwB9wmWzPN2Oeg==tXdWDKvN3PqAkqC2";
 
 export default function Home() {
+  const [quote, setQuote] = useState<string | null>(null);
+  const [category, setCategory] = useState<string>("");
+
+  const fetchQuote = async ({ category }: { category: string }) => {
+    const responsePromise = await fetch(
+      `https://api.api-ninjas.com/v1/quotes?category=${category}`,
+      {
+        method: "GET", // *GET, POST, PUT, DELETE, etc.
+        mode: "cors", // no-cors, *cors, same-origin
+        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: "same-origin", // include, *same-origin, omit
+        headers: {
+          "Content-Type": "application/json",
+          "X-Api-Key": API_KEY,
+        },
+        redirect: "follow", // manual, *follow, error
+        referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+      }
+    );
+    const responseParsed = await responsePromise.json();
+    setQuote(responseParsed[0].quote);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <div className="wrapper">
+      <p className="text-[24px] font-medium mt-24">
+        Inspirational Quote Generator
+      </p>
+      <p className="text-[18px] mt-12">Select a category</p>
+      <div className="grid grid-cols-4 gap-4 mt-8">
+        {CATEGORIES.map((category) => {
+          return (
+            <div
+              key={category.key}
+              className="text-[12px] rounded-xl bg-[#e4f5e98c] text-[#848484] py-2 px-4 items-center text-center cursor-pointer hover:bg-[#c6d9cc8c]"
+              onClick={() => {
+                setCategory(category.key);
+                fetchQuote({ category: category.key });
+              }}
+            >
+              {category.name}
+            </div>
+          );
+        })}
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="w-[770px] h-[220px] mt-24">
+        {/* <p>{category.toLocaleUpperCase()}</p> */}
+        <p className="mt-8 typewriter">{quote}</p>
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+      {quote !== null && (
+        <button
+          onClick={() => fetchQuote({ category })}
+          className="bg-[#e4f5e98c] hover:bg-blue text-[#848484] py-4 px-4 rounded-lg"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+          Regenerate
+        </button>
+      )}
+    </div>
+  );
 }
